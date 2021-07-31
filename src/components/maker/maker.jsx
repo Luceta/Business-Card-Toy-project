184ce02c843febed419b7d911514303c206f5a90
Luceta <57941164+Luceta@users.noreply.github.com>
2021-07-31 16:57:30 +0900

    ADD: card editor ui

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,30 +1,69 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import Footer from "../footer/footer";
-import Header from "../header/header";
-import styles from "./maker.module.css";
-import Editor from "../editor/editor";
-import Preview from "../prview/cardPreview";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
-    authService.onAuthChange((user) => {
+    authService.onAuthChange(user => {
       if (!user) {
-        history.push("/");
+        history.push('/');
       }
     });
   });
+
+  const addCard = card => {
+    console.log(card);
+  };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} addCard={addCard} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
