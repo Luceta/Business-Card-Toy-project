aec2b03557d51a8a262eb32ad99f5fbaad175842
Luceta <57941164+Luceta@users.noreply.github.com>
2021-07-31 13:31:21 +0900

    ADD: fix and editor component

---
+++ src/components/prview/cardPreview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./preview.module.css";
+const Preview = (props) => (
+  <section className={styles.preview}>
+    <div>
+      <h1 className={styles.title}>Preview</h1>
+    </div>
+  </section>
+);
+
+export default Preview;
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,11 +1,12 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_LEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
